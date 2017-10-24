6c6e74ea0a3103859a180b55297d5923aa4cf63f
tintedsky <hongbonew@gmail.com>
2017-10-24 16:45:03 -0400

    Initial Commit

---
+++ L219Objective-C_Interoperability/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  L219Objective-C_Interoperability
+//
+//  Created by Hongbo Niu on 2017-10-24.
+//  Copyright © 2017 Udemy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ L219Objective-C_Interoperability/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  L219Objective-C_Interoperability
+//
+//  Created by Hongbo Niu on 2017-10-24.
+//  Copyright © 2017 Udemy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
